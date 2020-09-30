da8a4736c22ee0413a9ffce5e3df07d1b0492a10
kdoh52 <kdoh52@gmail.com>
2020-09-30 12:35:31 -0700

    initial

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,18 @@
 let mysql = require("mysql");
 
-let connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "1234",
-    database: "burgers_db"
-});
+let connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        port: 3306,
+        user: 'root',
+        password: '1234',
+        database: 'burgers_db'
+    });
+};
 
 connection.connect(function(err) {
     if (err) {
